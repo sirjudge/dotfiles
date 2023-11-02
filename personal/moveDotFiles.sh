9f4e6fcbd553ea7f1b77a725d293247bced8732f
nicholas.judge <nicholas.judge@shareasale.com>
2023-11-02 12:19:38 -0400

    updated dotfiles

--- personal/moveDotFiles.sh
+++ personal/moveDotFiles.sh
@@ -4,17 +4,16 @@ if [ -d "/nvim" ]; then
     rm -r nvim
 fi
 
-
 echo "copying nvim files in to repo"
 cp -r ~/.config/nvim .
 
 echo "copying zshrc"
 cp ~/.zshrc .
 
-
 echo "push to git"
 git pull
 git add .
 git commit -m "update dotfiles from script"
 git push
+
 
