bb8320515ed600a8879369470bbb0bde76aa6a36
nicholas.judge <nicholas.judge@shareasale.com>
2024-04-09 14:56:30 -0400

    added zipping as first stage in copying dotfiles out

--- personal/moveDotFiles.sh
+++ personal/moveDotFiles.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+echo "zipping current config folder"
+zip -r config.zip ~/.config
+
 echo "clearing dotfile repo directory"
 if [ -d "/nvim" ]; then
     rm -r nvim
 fi
-
 echo "copying nvim files in to repo"
 cp -r ~/.config/nvim .
 
