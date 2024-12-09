405c68568988bd4f18e564322fff740b6fe3592b
nico <nico.a.judge@gmail.com>
2024-12-09 07:19:09 -0500

    added missing new monitor from other office monitor setting

--- personal/scripts/Monitor.sh
+++ personal/scripts/Monitor.sh
@@ -64,11 +64,10 @@ fi
 #| LG |
 #| LG | |Laptop|
 if [ "$action" = "on" ] && [ "$setup" = "officeDouble" ]; then
-    xrandr --output eDP-1-1 --mode 1920x1080 --rate 144.00 --rotate normal \
+    xrandr \
+        --output eDP-1-1 --mode 1920x1080 --rate 144.00 --rotate normal \
         --output DP-0.1 --mode 2560x1080 --left-of eDP-1-1 \
         --output DP-0.2 --mode 2560x1080 --below DP-0.1 --verbose
-
-# Laptop | Acer Predator | Alienware
 elif  [ "$action" = "on" ] && [ "$setup" = "officeMain" ]; then
 
     xrandr \
@@ -76,17 +75,17 @@ elif  [ "$action" = "on" ] && [ "$setup" = "officeMain" ]; then
         --output DP-0.2 --mode 1920x1080 --rate 239.76 --pos 4480x0 \
         --output DP-0.3 --mode 1920x1080 --rate 60.00 --pos 0x-1080 \
         --output DP-0.1 --mode 2560x1440 --rate 144.00 --pos 1920x0 --primary \
-
-
-# Laptop | Acer Predator | Alienware
 # ( but in 1080p)
 elif  [ "$action" = "on" ] && [ "$setup" = "officeGaming1080" ]; then
-    xrandr --output eDP-1-1 --mode 1920x1080 --rate 144.00 --pos 0x0 --primary \
-       --output DP-0.1 --mode 1920x1080 --pos 1920x0 \
-       --output DP-0.2 --mode 1920x1080 --rate 239.76 --pos 3840x0
+    xrandr \
+        --output eDP-1-1 --mode 1920x1080 --rate 144.00 --pos 0x0 --primary \
+        --output DP-0.1 --mode 1920x1080 --pos 1920x0 \
+        --output DP-0.2 --mode 1920x1080 --rate 239.76 --pos 3840x0 \
+        --output DP-0.3 --mode 1920x1080 --rate 60.00 --pos 0x-1080 \
 # | Laptop | Acer |
 elif [ "$action" = "on" ] && [ "$setup" = "bedroom" ]; then
-    xrandr --output eDP-1-1 --mode 1920x1080 \
+    xrandr \
+        --output eDP-1-1 --mode 1920x1080 \
         --output HDMI-0 --primary --mode 1920x1080 --right-of eDP-1-1
 fi
 
