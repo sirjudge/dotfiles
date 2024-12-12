94da68405606a41990d4f21df536d0b0f6f4b5a6
nicholas.judge <nicholas.judge@shareasale.com>
2024-12-11 22:20:48 -0500

    updated some more dotfiles

--- shared/nvim/lua/plugins/neotree.lua
+++ shared/nvim/lua/plugins/neotree.lua
@@ -21,9 +21,6 @@ return {
                         hide_dotfiles = false,
                         hide_gitignored = true,
                         hide_by_name = {
-                            -- '.git',
-                            -- '.DS_Store',
-                            -- 'thumbs.db',
                         },
                         never_show = {},
                     },
