a78dcf94af7b75093d714dda59ee3fc512c29a28
nico <nico.a.judge@gmail.com>
2024-12-18 08:17:01 -0500

    updated backup to be interactive :)

--- backupManager.sh
+++ backupManager.sh
@@ -1,21 +1,85 @@
 #!/bin/bash
 # Configure new line and take in command line arguments
 NEWLINE=$'\n'
-while getopts 'a:s:c:so' flag; do
+while getopts 'a:s:c:sohi' flag; do
     case "${flag}" in
         a) action="${OPTARG}" ;;
         s) setup="${OPTARG}" ;;
         c) clean="${OPTARG}" ;;
-        so) sourceBackup="${OPTARG}" ;;
+        o) sourceBackup="${OPTARG}" ;;
+        i) interactive=1 ;;
         h) echo\
+            "i => [i]nteractive"\
             "a => [a]ction (restore/backup)${NEWLINE}"\
             "s => [s]etup to work on (office/personal)${NEWLINE}"\
             "c => [c]lean existing files and folders${NEWLINE}"\
-            "so => [so]urce list backup${NEWLINE}"\
+            "o => s[o]urce list backup${NEWLINE}"\
             "example Usage: Monitor.sh -a  backup -s office -c true"
             exit 1 ;;
+        *) echo "Invalid flag provided, exiting"; exit 1 ;;
     esac
 done
+
+# Validation if interaactive flag is not set
+if [ $interactive = 0 ]; then
+    if [ "$action" = "" ]; then
+        echo "Please provide an action to perform using the -a flag: '-a {restore/backup}'${NEWLINE}"
+        exit 1
+    fi
+
+    if [ "$setup" = "" ]; then
+        echo "Please provide a setup to work on using the -s flag: '-s {office/personal}'${NEWLINE}"
+        exit 1
+    fi
+fi
+
+# Validation if interactive is set
+
+if [ $interactive = 1 ]; then
+    echo "Enter the setup to work on ([o]ffice/[p]ersonal):"
+    read setup_input -r
+    if [ "$setup_input" = "o" ]; then
+        setup="office"
+    elif [ "$setup_input" = "p" ]; then
+        setup="personal"
+    else
+        echo "Invalid setup provided, exiting"
+        exit 1
+    fi
+
+    echo "Enter the action to perform ([r]estore/[b]ackup):"
+    read action_input -r
+    if [ "$action_input" = "r" ]; then
+        action="restore"
+    elif [ "$action_input" = "b" ]; then
+        action="backup"
+    else
+        echo "Invalid action provided, exiting"
+        exit 1
+    fi
+
+    echo "Do you want to clean existing files and folders? ([y]es/[n]o)"
+    read clean_input -r
+    if [ "$clean_input" = "true" ]; then
+        clean="true"
+    elif [ "$clean_input" = "false" ]; then
+        clean="false"
+    else
+        echo "Invalid clean input provided, exiting"
+        exit 1
+    fi
+
+    echo "Do you want to backup the source list? (yes/no)"
+    read sourceBackup_input -r
+    if [ "$sourceBackup_input" = "y" ]; then
+        sourceBackup="true"
+    elif [ "$sourceBackup_input" = "n" ]; then
+        sourceBackup="false"
+    else
+        echo "Invalid source backup input provided, exiting"
+        exit 1
+    fi
+fi
 
 # Validation
 if [ "$setup" = "" ]; then
@@ -163,14 +227,14 @@ currentPath=$(pwd)
 if [ "$action" = "backup" ]; then
     if [ -n "$(git status --porcelain)" ]; then
         echo "Changes detected, do you want to push changes to git? (y/n)"
-        read gitPush
+        read gitPush -r
         if [ "$gitPush" = "y" ]; then
             echo "pushing changes to git"
             cd ~/solutions/dotfiles
             git add .
             git commit -m "Automated file backup"
             git push
-            cd $currentPath
+            cd "$currentPath"
         fi
     else
         echo "no changes detected, exiting"
@@ -179,3 +243,4 @@ fi
 
 echo "$action $setup completed"
 exit 1
+
