3f2c705b879e891d3e3fd1b62b18e9c026cd9dbf
Nico Judge <nico.a.judge@gmail.com>
2025-02-21 11:30:32 -0500

    fixed layout of waybar

--- home-manager/waybar/waybar.nix
+++ home-manager/waybar/waybar.nix
@@ -1,4 +1,3 @@
-
 { config, pkgs, lib, ... }:
 let 
 waybar_style = builtins.readFile ./style.css;
@@ -8,80 +7,83 @@ in
     enable = true;
     systemd.enable = true;
     style = waybar_style;
-    settings = [
-    {
-      output = ["DP-6"];
-      height = 30;
-      layer = "top";
-      position = "bottom";
-      tray = { spacing = 10; };
-      modules-center = [ "hyprland/window" ];
-      modules-left = [ "hyprland/workspaces" "hyprland/submap" ];
-      modules-right = [
-        "privacy"
-        "pulseaudio"
-        "network"
-        "cpu"
-        "memory"
-        "temperature"
-        "battery" 
-        "clock"
-        "tray"
-      ];
-      battery = {
-        format = "{capacity}% {icon}";
-        format-alt = "{time} {icon}";
-        format-charging = "{capacity}% ";
-        format-icons = [ "" "" "" "" "" ];
-        format-plugged = "{capacity}% ";
-        states = {
-          critical = 15;
-          warning = 30;
+    settings = 
+      [
+      {
+        output = ["DP-6"];
+        height = 30;
+        layer = "top";
+        position = "bottom";
+        tray = { spacing = 10; };
+        modules-center = [
+          "hyprland/workspaces" "hyprland/submap"
+        ];
+        modules-left = [
+          "hyprland/window" "privacy" "tray" 
+        ];
+        modules-right = [
+          "pulseaudio" "network" "cpu" "memory" "temperature" "battery" "clock"
+        ];
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
         };
-        #tooltip-format = "{capacity}% {icon} {time}";
-      };
-      clock = {
-        format-alt = "{:%Y-%m-%d}";
-        tooltip-format = "{:%Y-%m-%d | %H:%M}";
-      };
-      cpu = {
-        format = "{usage}% ";
-        tooltip = false;
-      };
-      memory = { format = "{}% "; };
-      network = {
-        interval = 1;
-        format-alt = "{ifname}: {ipaddr}/{cidr}";
-        format-disconnected = "Disconnected ⚠";
-        format-ethernet = "{ifname}: {ipaddr}/{cidr} 󰈀  up: {bandwidthUpBits} down: {bandwidthDownBits}";
-        format-linked = "{ifname} (No IP) 󰤭 ";
-        format-wifi = "{essid} ({signalStrength}%)  ";
-      };
-      pulseaudio = {
-        format = "{volume}% {icon} {format_source}";
-        format-bluetooth = "{volume}% {icon} {format_source}";
-        format-bluetooth-muted = " {icon} {format_source}";
-        format-icons = {
-          car = " ";
-          default = [ "" " " " " ];
-          handsfree = " ";
-          headphones = " ";
-          headset = " ";
-          phone = " ";
-          portable = " ";
+        battery = {
+          format = "{capacity}% {icon}";
+          format-alt = "{time} {icon}";
+          format-charging = "{capacity}% ";
+          format-icons = [ "" "" "" "" "" ];
+          format-plugged = "{capacity}%  ";
+          states = {
+            critical = 15;
+            warning = 30;
+          };
         };
-        format-muted = " {format_source}";
-        format-source = "{volume}% ";
-        format-source-muted = "";
-        on-click = "pavucontrol";
-      };
-      "hyprland/submap" = { format = ''<span style="italic">{}</span>''; };
-      temperature = {
-        critical-threshold = 80;
-        format = "{temperatureC}°C {icon}";
-        format-icons = [ "" "" "" ];
-      };
-    }
+        clock = {
+          format-alt = "{:%Y-%m-%d}";
+          tooltip-format = "{:%Y-%m-%d | %H:%M}";
+        };
+        cpu = {
+          format = "{usage}% ";
+          tooltip = false;
+        };
+        memory = { format = "{}% "; };
+        network = {
+          interval = 1;
+          format-alt = "{ifname}: {ipaddr}/{cidr}";
+          format-disconnected = "Disconnected ⚠";
+          format-ethernet = "{ifname}: {ipaddr}/{cidr} 󰈀  up: {bandwidthUpBits} down: {bandwidthDownBits}";
+          format-linked = "{ifname} (No IP) 󰤭 ";
+          format-wifi = "{essid} ({signalStrength}%)  ";
+        };
+        pulseaudio = {
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-icons = {
+            car = " ";
+            default = [ "" " " " " ];
+            handsfree = " ";
+            headphones = " ";
+            headset = " ";
+            phone = " ";
+            portable = " ";
+          };
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          on-click = "pavucontrol";
+        };
+        "hyprland/submap" = { format = ''<span style="italic">{}</span>''; };
+        temperature = {
+          critical-threshold = 80;
+          format = "{temperatureC}°C {icon}";
+          format-icons = [ "" "" "" ];
+        };
+      }
     {
       output = ["DP-7"];
       height = 30;
