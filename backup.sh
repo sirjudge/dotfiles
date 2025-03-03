b98e2c17ad1e2d284be5125cd418f45ecfe4eabc
Nico Judge <nico.a.judge@gmail.com>
2025-03-03 09:37:55 -0500

    updated layout to personal/work folders

--- backup.sh
+++ backup.sh
@@ -8,15 +8,15 @@ while getopts "bi" opt; do
 done
 
 if [ "$backup" = true ]; then
-    cp -r /etc/nixos .
-    cp -r ~/.config/home-manager/ .
-    cp -r ~/.config/nixpkgs .
-    cp -r ~/.config/nvim .
+    cp -r /etc/nixos ./personal/.
+    cp -r ~/.config/home-manager/ ./personal/.
+    cp -r ~/.config/nixpkgs ./personal/.
+    cp -r ~/.config/nvim ./personal/.
 fi
 
 if [ "$insert" = true ]; then
-  cp -r nixos /etc/
-  cp -r home-manager ~/.config/
-  cp -r nvim ~/.config/
+  cp -r ./personal/nixos /etc/
+  cp -r ./personal/home-manager ~/.config/
+  cp -r ./personal/nvim ~/.config/
 fi
 
