8aa506b4a2ca9813a25627b4fe1b8e9710e06d81
Nico Judge <nico.a.judge@gmail.com>
2025-04-13 10:49:18 -0400

    updated config path

--- personal/nixos/flake.nix
+++ personal/nixos/flake.nix
@@ -36,7 +36,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.nico = import ./home/surface/home.nix;
+          home-manager.users.nico = import ./home/razer/home.nix;
         }
       ];
     };
