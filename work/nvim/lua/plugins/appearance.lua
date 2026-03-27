5f0b19a308f08e09745cfc09315aedaa817ec420
Nico Judge <njudge@ntracts.com>
2026-03-27 09:02:13 -0400

    added codecompanion.lua file to try out new plugin, made small tweak to appearance and lazy lua files to remove excess whitespace

--- work/nvim/lua/plugins/appearance.lua
+++ work/nvim/lua/plugins/appearance.lua
@@ -1,45 +1,45 @@
 return {
-    -- makes cursor be all smooth like
-    {
-        "gen740/SmoothCursor.nvim",
-        config = function()
-            require("smoothcursor").setup()
-        end,
-    },
-    {
-        'zeybek/camouflage.nvim',
-        event = { 'BufReadPre', 'BufNewFile' },
-        opts = {
-            enabled = true,
-            auto_enable = true,
-            max_lines = 5000,
-            -- Parser settings
-            parsers = {
-                include_commented = true,      -- Include commented lines (all parsers)
-                env = {
-                    include_export = true,       -- Include export KEY=value
-                },
-                json = {
-                    max_depth = 10,              -- Maximum nesting depth
-                },
-                yaml = {
-                    max_depth = 10,              -- Maximum nesting depth
-                },
-            },
-            -- Integrations
-            integrations = {
-                telescope = true,
-                cmp = {
-                    disable_in_masked = true,
-                },
-            },
-        },
-        keys = {
-            { 
-                '<leader>ct',
-                '<cmd>CamouflageToggle<cr>',
-                desc = 'Toggle Camouflage' 
-            },
-        },
-    },
+	-- makes cursor be all smooth like
+	{
+		"gen740/SmoothCursor.nvim",
+		config = function()
+			require("smoothcursor").setup()
+		end,
+	},
+	{
+		"zeybek/camouflage.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			enabled = true,
+			auto_enable = true,
+			max_lines = 5000,
+			-- Parser settings
+			parsers = {
+				include_commented = true, -- Include commented lines (all parsers)
+				env = {
+					include_export = true, -- Include export KEY=value
+				},
+				json = {
+					max_depth = 10, -- Maximum nesting depth
+				},
+				yaml = {
+					max_depth = 10, -- Maximum nesting depth
+				},
+			},
+			-- Integrations
+			integrations = {
+				telescope = true,
+				cmp = {
+					disable_in_masked = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>ct",
+				"<cmd>CamouflageToggle<cr>",
+				desc = "Toggle Camouflage",
+			},
+		},
+	},
 }
